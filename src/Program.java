a5e8a59c3365bc0de541fdf1bbaaf1ee6a73de41
Ehsan Ali <ehsan27ali@gmail.com>
2020-11-22 14:08:54 -0600

    adding mainMenu method

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
 	}
-
+	
+	public static int mainMenu() {
+		
+		char userInput;
+		Scanner input = new Scanner(System.in);
+		
+		do {
+			System.out.println("\nUser Account Database");
+			System.out.println("=====================");
+			System.out.println("1) Create New Account");
+			System.out.println("2) Sign In");
+			System.out.print("User input: ");
+			userInput = input.next().charAt(0);
+			
+			if(userInput != '1' && userInput != '2')
+				System.out.println("\nERROR. Invalid user input.");
+			
+		}while(userInput != '1' && userInput != '2');
+		
+		input.close();
+		int userInput_int = userInput - '0';
+		return userInput_int;
+	}
+	
 }
